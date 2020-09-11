fb24e2108ec0fb82f50b6e273a5c5f28689b19db
renzoIrusta <renzoirusta22@gmail.com>
2020-09-11 12:43:09 -0300

    docs agregado

---
+++ docs/precache-manifest.5ad54cdb8b66c9c280482382949513ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8562ed50aa6ba4c89941b69a8797821",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9262e9200afa10616350",
+    "url": "/static/css/main.aa115e0b.chunk.css"
+  },
+  {
+    "revision": "f282757b0c05702f74d8",
+    "url": "/static/js/2.b1f59dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1f59dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9262e9200afa10616350",
+    "url": "/static/js/main.f4119617.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
